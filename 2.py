d0033473e22346230f9b673ca3a135ad936955ea
전은형 <ooooo5h91@gmail.com>
2022-02-05 23:37:43 +0900

    zfill()
    - zfill(n)  : n만큼 자리수를 채워주는 함수

--- 2.py
+++ 2.py
@@ -1,14 +1,16 @@
 # 2018 카카오 코딩테스트  
 
-print(bin(9))  # 0b1001   9를 이진수로 나타내기
-print(bin(30))  # 0b11110   30를 이진수로 나타내기
-print(bin(9)[2:])  
-print(bin(30)[2:])  
+# print(bin(9))  # 0b1001   9를 이진수로 나타내기
+# print(bin(30))  # 0b11110   30를 이진수로 나타내기
+# print(bin(9)[2:])  
+# print(bin(30)[2:])  
 
-print(bin(9|30)[2:].replace('1', '#').replace('0', ' '))  # 9와 30을 이진수로 변환하고 1를 #, 0을 ' '으로 바꾸기
+# print(bin(9|30)[2:].replace('1', '#').replace('0', ' '))  # 9와 30을 이진수로 변환하고 1를 #, 0을 ' '으로 바꾸기
 
 arr1 = [3,20,28,18,11]
 arr2 = [5,1,21,17,28]
 
+n = 5
+
 for i,j in zip(arr1, arr2):   
-    print(bin(i|j)[2:].replace('1', '#').replace('0', ' ')) # 자리수가 안맞아서 자리수를 맞춰줘야함
+    print(bin(i|j)[2:].zfill(5).replace('1', '#').replace('0', ' '))  # zfill(n)  => n만큼 자리수를 채워주는 함수
