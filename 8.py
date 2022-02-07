8cb808b1c90f6e575bbd46ec258f7d1c321f0dec
전은형 <ooooo5h91@gmail.com>
2022-02-07 18:06:01 +0900

    from itertools import combinations, combinations 사용해서 푸는 방법

--- 8.py
+++ 8.py
@@ -1,17 +1,14 @@
 # 프로그래머스 두개 뽑아서 더하기
+from itertools import combinations
 
 def solution(numbers):
-    
-    # 중복을 피해주는 set으로 생성하기
-    answer = set()
-    
-    for i in range( len(numbers) ):
-        for j in range(i+1, len(numbers)):
-            answer.add(numbers[i] + numbers[j])
-            # set()은 append가 아니라 add!
-        
-    # set을 list로 변환한뒤 정렬해주기
-    answer = list(answer)
-    answer.sort()
 
-    return answer
+    return sorted(list(set([sum([i,j]) for i, j in combinations(numbers, 2)])))
+
+ 
+    # 1. combinations(numbers, 2) => numbers 리스트에서 2개씩 묶은 모든 조합 가져오기
+    # 2. [sum([i,j]) for i, j in combinations(numbers, 2)]  => 리스트 컴프리헨션
+    # 3. set([sum([i,j]) for i, j in combinations(numbers, 2)])  => set()은 중복 제거
+    # 4. list(set([sum([i,j]) for i, j in combinations(numbers, 2)])) => set()을 list()로 변경
+    # 5. sorted(list(set([sum([i,j]) for i, j in combinations(numbers, 2)])))  => sorted()로 순서대로 정렬
+    # 아름답다 코드 한 줄.. 
