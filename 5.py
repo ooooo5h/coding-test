3331ecec3d6452f31fd978438d6fdf11b91f7c75
전은형 <ooooo5h91@gmail.com>
2022-02-07 10:42:04 +0900

    홀/짝에 따른 승패를 결정
    중간 단계를 출력하면서 상황 파악하기

--- 5.py
+++ 5.py
@@ -16,4 +16,29 @@ while True:
         continue  # 반복문의 이번 바퀴만 skip하는 명령어
         
     # 제대로 입력을 했다면 마저 진행하자
-    print('홀/짝 판별 차례')
+    # 판별하기 전에, cpu가 갖고있는 구슬의 갯수를 공개해주자
+    print(f"컴퓨터가 쥔 구슬 : {cpu_count}")
+    
+    if user_answer == '홀':
+        if cpu_count % 2 == 1:
+            
+            print('사용자가 승리')
+            # 사용자가 승리 코인 한개 cpu로 부터 받아오자
+            user_coin += 1
+            cpu_count -= 1
+        else:
+            print('사용자가 땡')
+            user_coin -= 1
+            cpu_coin += 1
+    else:
+        # 짝을 입력한 경우
+        if cpu_count % 2 == 0:
+            print('사용자가 승리')
+            # 맞춘 경우
+            user_coin += 1
+            cpu_coin -= 1
+            
+        else:
+            print('사용자가 땡')
+            user_coin -= 1
+            cpu_coin += 1
