70eb653f104938ebbfa2293a3c9256bcb0c98121
전은형 <ooooo5h91@gmail.com>
2022-02-07 11:04:15 +0900

    다른 풀이

--- 5.py
+++ 5.py
@@ -1,66 +1,81 @@
 import random
 
-# 추가 문제 => 코인을 각각 20개씩 갖고 시작하자
-# 홀/짝에 몇개를 배팅할지 1~5 사이로 입력을 제한하자
-# ===> 내가 가진 코인 갯수보다 많이 배팅은 불가하다
-# 배팅한 갯수만큼의 코인이 이동하도록
-
+# 추가 문제 => 코인을 각각 20개씩 갖고 시작.
+#  홀/짝에 몇개를 베팅할지 1~5 사이로 입력 제한.
+#    => 내가 가진 코인 갯수보다 많이 베팅은 불가.
+#  베팅한 갯수 만큼 코인이 이동하도록.
 
 user_coin = 20
 cpu_coin = 20
 
 while True:
     
-    gamble_count = int(input('1~5사이에서 배팅을 하고 싶은 구슬의 숫자를 입력하세요 : '))
+    # 컴퓨터가 1~6개 사이의 구슬을 세팅함.
+    cpu_count = random.randint(1, 6)
     
-    if gamble_count > user_coin:
-        print('갖고있는 구슬의 갯수가 배팅하려는 구슬의 갯수보다 적어서 게임진행이 불가합니다.')
-    elif gamble_count not in range(1, 6):
-        print('1에서 5 사이의 구슬만 배팅하세요.')
-    else : 
-            
-        cpu_count = random.randint(1, 6)
+    # 홀/ 짝인지 답을 입력.
+    
+    user_answer = input('홀 / 짝을 맞춰주세요 : ')
+    
+    if user_answer not in ['홀', '짝']:
+        print("잘못된 입력입니다.")
+        continue  # 반복문의 이번 바퀴만 skip
         
-        # 홀/짝?
-        user_answer = input('홀/짝 뭐게 : ')
         
-        if user_answer not in ['홀', '짝']:
-            print('잘못된 입력입니다.')
-            continue  # 반복문의 이번 바퀴만 skip하는 명령어
-            
-        user_bet_coint = int(input('1~5사이에서 배팅을 하고 싶은 구슬의 숫자를 입력하세요 : '))   
-            
-            
-        # 제대로 입력을 했다면 마저 진행하자
-        # 판별하기 전에, cpu가 갖고있는 구슬의 갯수를 공개해주자
-        print(f"컴퓨터가 쥔 구슬 : {cpu_count}")
+    # 몇개의 코인을 베팅할지.
+    user_bet_coin = int(input('몇개의 코인을 베팅? 1~5 사이로 : '))
+    
+    if user_bet_coin not in range(1, 6):  # 1~ 6직전 : 1~5 사이에 없는지?
+        print('1~5개 사이만 베팅 가능합니다.')
+        continue
+    
+    if user_bet_coin > user_coin:  # 내가 가진 코인보다 더 많이 베팅함.
+        print('보유 코인이 부족합니다.')
+        continue
+    
+    
+    if user_bet_coin > cpu_coin:  # CPU가 코인 부족.
+        print('상대방의 보유 코인이 부족합니다.')
+        continue
         
-        if user_answer == '홀':
-            if cpu_count % 2 == 1:
-                
-                print('정답')
-                user_coin += gamble_count
-                cpu_count -= gamble_count
-            else:
-                print('땡')
-                user_coin -= gamble_count
-                cpu_coin += gamble_count
+    # 제대로 입력햇다면 마저 진행.
+    
+    # CPU가 몇개를 집었는지 공개
+    print(f'CPU는 {cpu_count}개의 구슬을 집었습니다.')
+    
+    if user_answer == '홀':
+        if cpu_count % 2 == 1:
+            
+            print('사용자 승리입니다.')
+            
+            # 사용자 승리. => 코인 한개 CPU로부터 받아오자.
+            user_coin += 1
+            cpu_coin -= 1
         else:
-            if cpu_count % 2 == 0:
-                print('정답')
-                user_coin += gamble_count
-                cpu_coin -= gamble_count
-                
-            else:
-                print('땡')
-                user_coin -= gamble_count
-                cpu_coin += gamble_count
-        
-        if user_coin == 0:
-            print('사용자의 코인이 모두 떨어졌습니다.')
-            print('GAME OVER')
-            break
-        elif cpu_coin == 0:
-            print('cpu의 코인이 모두 떨어졌습니다.')
-            print('YOU WIN')
-            break
+            print('사용자 패배입니다.')
+            user_coin -= 1
+            cpu_coin += 1
+    else:
+        # 짝을 입력한 경우.
+        if cpu_count % 2 == 0:
+            
+            print('사용자 승리입니다.')
+            # 맞춘 경우.
+            user_coin += 1
+            cpu_coin -= 1
+        else:
+            
+            print('사용자 패배입니다.')
+            user_coin -= 1
+            cpu_coin += 1
+            
+    # 둘중 하나의 코인이 다 떨어졌다면?  경기 종료
+    
+    if user_coin == 0:
+        print('사용자의 코인이 바닥났습니다.')
+        print('GAME OVER')
+        break
+    elif cpu_coin == 0:
+        print('CPU의 코인이 바닥났습니다.')
+        print('You win!')
+        break
