3546614e3d27b5b7c35621d74c6fb8e3cecf022b
전은형 <ooooo5h91@gmail.com>
2022-02-07 11:52:28 +0900

    2초마다 자동으로 모니터링하는 기능을 추가

--- 6.py
+++ 6.py
@@ -1,57 +1,53 @@
 # requests 라이브러리를 활용해서 API를 연계해보자
 # 한국의 BTC 시세 VS 해외의 BTC 시세를 비교해보자
-from urllib import request
+from time import sleep
 import requests    # 가상환경에서 시작해야함!! pip install requests
+import datetime
 
-# 업비트의 현재 시세 조회하는 API 호출
-upbit_response = requests.get('https://api.upbit.com/v1/ticker?markets=KRW-BTC')
+while True:
+    
+    # 자동 모니터링 시키자
+    print(f'현재 일시 : {datetime.datetime.now()}')
 
-upbit_response_json = upbit_response.json()
+    # 업비트의 현재 시세 조회하는 API 호출
+    upbit_response = requests.get('https://api.upbit.com/v1/ticker?markets=KRW-BTC')
 
-upbit_btc_price = upbit_response_json[0]['trade_price']
+    upbit_response_json = upbit_response.json()
 
-print(f'국내 Upbit의 BTC 가격 : {upbit_btc_price}원') 
+    upbit_btc_price = upbit_response_json[0]['trade_price']
 
-  
-# 바이낸스의 현재 시세 조회 -> $로 리턴됨 * 환율로, 원화로 만들어서 업비트와 비교가 가능함
-binance_response = requests.get('https://api.binance.com/api/v1/ticker/allPrices')
-binance_json = binance_response.json()
+    print(f'국내 Upbit의 BTC 가격 : {upbit_btc_price}원') 
 
-# binance_json은 일종의 리스트로 되어있어서 반복문을 돌리면서 꺼내야함
-for coin_item in binance_json:
-    if coin_item['symbol'] == 'BTCUSDT':
-        # print(coin_item)
-        binance_btc_price_dollar = float(coin_item['price'])
-        
-print(f'해외 Binance의 BTC 가격 : {binance_btc_price_dollar}')
+    
+    # 바이낸스의 현재 시세 조회 -> $로 리턴됨 * 환율로, 원화로 만들어서 업비트와 비교가 가능함
+    binance_response = requests.get('https://api.binance.com/api/v1/ticker/allPrices')
+    binance_json = binance_response.json()
 
-# 현재 1달러당 원화로는 얼마인가? => API를 활용하자
+    # binance_json은 일종의 리스트로 되어있어서 반복문을 돌리면서 꺼내야함
+    for coin_item in binance_json:
+        if coin_item['symbol'] == 'BTCUSDT':
+            # print(coin_item)
+            binance_btc_price_dollar = float(coin_item['price'])
+            
+    print(f'해외 Binance의 BTC 가격 : {binance_btc_price_dollar}')
 
-dollar_response = requests.get('https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes=FRX.KRWUSD')
-dollar_json = dollar_response.json()
+    # 현재 1달러당 원화로는 얼마인가? => API를 활용하자
+    dollar_response = requests.get('https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes=FRX.KRWUSD')
+    dollar_json = dollar_response.json()
 
-dollar_to_won = dollar_json[0]['basePrice']
+    dollar_to_won = dollar_json[0]['basePrice']
 
-print(f'현재 환율 : {dollar_to_won}')
+    print(f'현재 환율 : {dollar_to_won}')
 
-binance_btc_price_to_won = binance_btc_price_dollar * dollar_to_won
-print(f'해외 Binance의 BTC 가격 : {binance_btc_price_to_won}')
+    binance_btc_price_to_won = binance_btc_price_dollar * dollar_to_won
+    print(f'해외 Binance의 BTC 가격 : {binance_btc_price_to_won}')
 
+    primenum = (upbit_btc_price - binance_btc_price_to_won) / binance_btc_price_to_won * 100
 
-# 프리미엄이 몇 % 붙어있는지 확인해보자
+    # 소수점 두자리까지만 표기
+    primenum = round(primenum, 2)
 
-# 라면 판매중
-
-# A가게   B가게
-# 1100    1000
-
-# 프리미엄 계산해보자 => A가게가 10% 더 비싸다는 결론은,
-#                      (A가게의 가격 - B가게의 가격) / B가게 가격 * 100
-
-
-primenum = (upbit_btc_price - binance_btc_price_to_won) / binance_btc_price_to_won * 100
-
-# 소수점 두자리까지만 표기
-primenum = round(primenum, 2)
-
-print(f'국내 BTC가 해외 BTC보다 {primenum}% 더 비싸요.')
+    print(f'국내 BTC가 해외 BTC보다 {primenum}% 더 비싸요.')
+    
+    # 2초 정지하고나서 재실행시키자
+    sleep(2)
