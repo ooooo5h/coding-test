721094df6d48407dd3d9a8e5f4afb318d8829f8c
전은형 <ooooo5h91@gmail.com>
2022-02-07 17:56:07 +0900

    이중반복문을 사용하면 풀리는 문제였다

--- 8.py
+++ 8.py
@@ -2,17 +2,16 @@
 
 def solution(numbers):
     
-    numbers = sorted(numbers)
-    print(numbers)
+    # 중복을 피해주는 set으로 생성하기
+    answer = set()
     
-    sum_list = []
-    sum = 0
-    index = 1
-    
-    for number in numbers:
-        sum = number + numbers[index]
-        sum_list.append(sum)
-        index += 1
-    
-    print(sum_list)
-    
+    for i in range( len(numbers) ):
+        for j in range(i+1, len(numbers)):
+            answer.add(numbers[i] + numbers[j])
+            # set()은 append가 아니라 add!
+        
+    # set을 list로 변환한뒤 정렬해주기
+    answer = list(answer)
+    answer.sort()
+
+    return answer
