bc969474ed56a42fb0145a55e8ba57b5fe3e9190
전은형 <ooooo5h91@gmail.com>
2022-02-08 10:37:11 +0900

    다른 풀이 => 딕셔너리 사용

--- 3.py
+++ 3.py
@@ -1,20 +1,18 @@
-# 로또의 최고 순위와 최저 순위
-
 def solution(lottos, win_nums):
     
-    rank = [6,6,5,4,3,2,1]
+    count = 0   
+    answer = []
+    dic = {6:1, 5:2, 4:3, 3:4, 2:5, 1:6, 0:6}   # 0:6을 추가해줘야지 정답이 제대로 리턴됨
     
-    count_zero = lottos.count(0)  # 첫바퀴때 2개
+    for lotto in lottos:
+        if win_nums.count(lotto) > 0:
+            count += 1
     
-    answer = 0
-    for x in win_nums:
-        if x in lottos:
-            answer += 1  # 맞춘 갯수 첫바퀴때 2개
-            
-    # print(rank[count_zero + answer], rank[answer] )
+    answer.append(dic[count + lottos.count(0)])
+    answer.append(dic[count])
     
-    return rank[count_zero + answer], rank[answer] 
-    
-    # count를 생각하긴 했는데 저렇게 rank에 담아서 하자는 생각이 안떠오름
-    # for문과 if문안에서 x를 같이 써도 되는데 그걸 생각을 못했음..!!
-    # 수학익힘책 푸는 느낌이다.. 대단해
+    return answer
+
+
+    # 0개 6등도 추가해줘야지 정상적으로 리턴이 되는데
+    # 왜냐 하나도 못맞출 수 있으니까!!
