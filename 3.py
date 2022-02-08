cd8fa1d56d8318a8b1e3ed397bbda6b4c8c36a56
전은형 <ooooo5h91@gmail.com>
2022-02-08 09:59:18 +0900

    로또 최저순위 최고순위 다시 풀기 실패

--- 3.py
+++ 3.py
@@ -2,42 +2,26 @@
 
 def solution(lottos, win_nums):
     
-    print('내 로또 번호' ,lottos)
-    print('당첨 로또 번호', win_nums)
+    print(sorted(lottos))
+    print(sorted(win_nums))
     
-    # 맞춘 숫자의 갯수를 알아야함
-    correct_num_count = 0
+    correct_num = 0
+    max_rank = 0  # 1등
+    min_rank = 0  # 6등
     
-    # 내 숫자를 들고 당첨 번호 안에 있는지 확인 6개의 숫자 전부 진행
-    for my_num in lottos:
-        if my_num in win_nums:
-            # 맞는 숫자 발견
-            correct_num_count += 1
+    for lotto in lottos:
+        for win_num in win_nums:
+            if lotto == win_num:
+                correct_num += 1 
     
+    print(correct_num)
     
-    # 맞춘 갯수? => 0이 있건 없건 최소한 몇개를 맞췄는지 구해주는 행위
-    print(f"맞춘 갯수 : {correct_num_count}")
+    if 0 in lottos:
+        zero_count = lottos.count(0)
+      
+    max_rank = 7 - correct_num + zero_count   # 갖고 있는 0의 갯수 + 일치한 갯수를 7에서 빼면 순위가 나옴 
+    min_rank = 7 - correct_num # 갖고 있는 0이 모두 틀리다는 가정 + 일치한 갯수를 7에서 빼면 최소순위
     
-    # 0이 있다면 => 추가로 맞출 여지가 있다는 의미. 0의 갯수만큼
-    # 9의 갯수를 구해보자
-    zero_count = 0
-    
-    for my_num in lottos:
-        if my_num == 0:
-            zero_count += 1
-            
-    # 최대로 맞출 수 있는 갯수는 correct_num_count + zero_count
-    max_correct_num_count = correct_num_count + zero_count
-    
-    # 등수 : 7 - 맞춘 갯수 => 등수가 6보다 크거나 같게 나오면, 6으로 변경해주자
-    # 최소로 맞췄을 때의 등수와 , 0도 다 맞췄을때의 등수를 구해서 answer에 넣어주면 정답이 되겠다
-    min_rank = 7 - correct_num_count
-    if min_rank > 6:
-        min_rank = 6
-    
-    max_rank = 7 - max_correct_num_count
-    if max_rank > 6:
-        max_rank = 6
     
     answer = [max_rank, min_rank]
     
