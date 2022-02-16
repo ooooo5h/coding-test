6a327eb57f966be5fcd107a3a569ec67e4b1cb99
전은형 <ooooo5h91@gmail.com>
2022-02-16 20:58:04 +0900

    다른 사람 풀이
    미쳤다 내가 모르는 개념 하나도 없는데 이렇게 간단하게 풀이도 가능하구나.. 형변환이 핵심 포인트인듯!!

--- 13.py
+++ 13.py
@@ -2,18 +2,4 @@
 
 def solution(x):
     
-    sum =0
-    original = x  # 내가 푼 풀이로는, x의 값이 몫으로 변해버리기 때문에 원래의 값을 저장해두는 게 문제였다.
-    
-    for i in range(len(str(x))):
-        
-        div_num = x // 10
-        add_num = x % 10
-        sum = sum + add_num
-        x = div_num
-    
-    if original % sum == 0:
-        return True
-    else :
-        return False
-
+    return x % sum([int(c) for c in str[x]]) == 0
