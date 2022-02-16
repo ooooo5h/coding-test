524eda3b9a7fd2d32817f09c0db8ea1c7241d9e5
전은형 <ooooo5h91@gmail.com>
2022-02-16 21:47:34 +0900

    풀이의 해결은 간단했따

--- 15.py
+++ 15.py
@@ -1,6 +1,5 @@
 # x만큼 간격이 있는 n개의 숫자
 def solution(x, n):
 
-    return list(range(x, x*n+1, x))
-    # x가 -일때는 오류가 난다
-    
+    return list(range(x, x*(n+1), x))
+    # x가 음수일때는 숫자가 줄어들기 때문.. n을 1 키워준 다음 곱해주면 되는데!! 아까워라
