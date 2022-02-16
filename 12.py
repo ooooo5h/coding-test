dbd8ea75bae354ae7b34d34bb87c33b39dbdac1b
전은형 <ooooo5h91@gmail.com>
2022-02-16 20:09:45 +0900

    다른 사람 풀이 미쳤..
    이렇게 간결하게도 작성 가능하구나..

--- 12.py
+++ 12.py
@@ -1,16 +1,5 @@
 # 수박수박수박수?
 
 def solution(n):
-    answer = '수박'
     
-    # n=3이면 3번 i 반복
-    if n % 2 == 0:
-        # n이 짝수
-        answer *= (n//2)
-    else : 
-        # n이 홀수
-        answer *= ((n-1)//2)
-        answer += '수'
-            
-    
-    return answer
+    return '수박'*(n//2) + '수'*(n%2)
