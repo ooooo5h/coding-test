af46a6afb6e5105bea789332e2626f3344df5646
전은형 <ooooo5h91@gmail.com>
2022-02-16 23:04:00 +0900

    내 풀이 성공
    문제에서 n는 int라고 주어졌는데 왜 굳이 int()로 감싸야 정답이 되는건지 모르겠다.

--- 18.py
+++ 18.py
@@ -1,7 +1,14 @@
-# 정수 내림차순으로 배치하기
-
 def solution(n):
+    answer = ''
+        
+    list_str = [i for i in str(int(n))]
 
-    ls = list(str(int(n)))
-    ls.sort(reverse = True)   # list자료형.sort(reverse=True)를 하면 역순으로 정렬이 된다
-    return int("".join(ls))   # "".join을 하면 list자료형이 str로 붙는다.
+    list_str.sort()
+    list_str.reverse()
+    
+    # print(list_str)
+    
+    for x in list_str :
+        answer += x
+    
+    return int(answer)
