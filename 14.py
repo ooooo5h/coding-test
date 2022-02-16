fb19a3535ad3ea80b64ee63efe68b21495e62f32
전은형 <ooooo5h91@gmail.com>
2022-02-16 21:03:59 +0900

    코드 줄이기

--- 14.py
+++ 14.py
@@ -1,13 +1,5 @@
-# 문자열 내 p와 y의 개수
+# 문자열 내 p와 y의 개수(코드 요약 도전)
 
 def solution(s):
-    
-    s = s.lower()
-    
-    count_s = s.count('p')
-    count_y = s.count('y')
-    
-    if count_s == count_y:
-        return True
-    else:
-        return False
+        
+    return s.lower().count('p') == s.lower().count('y')
