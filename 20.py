f851899f35394bfe1d3d05294fbd8ab29d587a0b
전은형 <ooooo5h91@gmail.com>
2022-02-17 11:47:20 +0900

    다른 사람 풀이
    재귀함수 이용!

--- 20.py
+++ 20.py
@@ -2,4 +2,9 @@
 
 def solution(n):
     
-    return sum(list(map(int, str(n))))
+    # 들어오는 n의 수가 일의자리 수라면 그대로 리턴
+    if n < 10:
+        return n;
+    
+    # 그 외의 숫자들은 재귀함수를 이용하여 풀었다.. 재귀함수 멋지다!! 0.0초로 시간이 줄어듬
+    return (n % 10) + solution(n // 10) 
