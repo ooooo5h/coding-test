93a2f816dbadfcdda86f46d20aef2850c35c4d75
전은형 <ooooo5h91@gmail.com>
2022-02-21 16:09:27 +0900

    코드 줄이기
    - for i,j,k in commands:
    를 실행하면 알아서 i,j,k에 값이 담기고 이를 이용할 수 있음

--- 22.py
+++ 22.py
@@ -4,17 +4,13 @@ def solution(array, commands):
     
     answer = []
 
-    for command in commands:
-        cutted_array = array[command[0]-1:command[1]]
+    for i,j,k in commands:
+        cutted_array = array[i-1:j]
     
         # 정렬하기
         cutted_array.sort()
-        # print(cutted_array)
         
-        # 정렬한 수에서 K번째 수 리턴
-        # print(cutted_array[command[2]-1])
-        
-        answer.append(cutted_array[command[2]-1])
+        answer.append(cutted_array[k-1])
         
     return answer
     
