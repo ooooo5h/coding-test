5140479549ba97d212ad8e2834090373ba7defff
전은형 <ooooo5h91@gmail.com>
2022-02-24 15:39:21 +0900

    리스트 풀이법

--- 25.py
+++ 25.py
@@ -1,30 +1,37 @@
 # 체육복
-
 def solution(n, lost, reserve):
     
-    # 여벌체육복이 있는 학생도 체육복을 도난당했을 수도 있다(중복된단 이야기)
-    # 진짜 여벌이 있는 애들
-    reserve_only = set(reserve) - set(lost)
+    # 1-0, n+1까지 있어야 하니까
+    student = [0] * (n+2)
     
-    # 진짜 체육복이 없는 애들
-    lost_only = set(lost) - set(reserve)
+    # 반복문을 돌면서 체육복없는 친구, 빌려줄 수 있는 친구 체크하기
+    # 체크하면서 자동으로 중복을 없애줌.
+    for l in lost:
+        student[l] -= 1
     
-    # 여벌이 있는 애들이 빌려줄 수 있는지 확인하기
-    for reserve in reserve_only:
-        front_student = reserve-1
-        back_student = reserve+1
+    for r in reserve:
+        student[r] += 1
+      
+    # 1부터 n까지 돌면서 확인해야함
+    for i in range(1, n+1):
+        # 여분 체육복이 있는 아이들만 체육복을 빌려주면 됨
+        if student[i] > 0:
+            front = i-1
+            back = i+1
+            if student[front] < 0 :
+                # 체육복 빌려줘야함
+                student[i] -= 1
+                student[front] += 1
+            elif student[back] < 0 :
+                student[i] -= 1
+                student[back] += 1
         
-        # 앞에 있는 학생이 체육복이 진짜로 없다면 빌려주기
-        # 빌려주면 체육복이 있게 되니까 없는 명단에서 삭제
-        if front_student in lost_only:
-            lost_only.remove(front_student)
-            
-        # 뒤에 있는 학생 빌려주면 명단에서 뒷번호 삭제
-        elif back_student in lost_only:
-            lost_only.remove(back_student)
-            
-    # 전체 학생수에서 잃어버린 명단에 있는 숫자를 빼주면 된다
-    return n - len(lost_only)
-            
+    # student에 담겨있는 값이 -1인 애들은 체육복 없어서 수업 불가
+    # 0, 1 은 수업 가능
+    answer = 0
     
+    for i in range(1, n+1):
+        if student[i] >= 0:
+            answer += 1
     
+    return answer
