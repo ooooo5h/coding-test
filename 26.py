cd678b7347bc614c1bfb62b5b6dd3c00fb68864f
전은형 <ooooo5h91@gmail.com>
2022-02-25 13:25:05 +0900

    풀이법

--- 26.py
+++ 26.py
@@ -2,34 +2,31 @@
 
 def solution(answers):
     
-    stu1 = [1,2,3,4,5] * len(answers)
-    stu2 = [2,1,2,3,2,4,2,5] * len(answers)
-    stu3 = [3,3,1,1,2,2,4,4,5,5] * len(answers)
-    
-    answer_list = []
-    count1, count2,count3 = 0,0,0
-    for index, answer in enumerate(answers) :
-        if answer == stu1[index]:
-            count1 += 1
-        if answer == stu2[index]:
-            count2 += 1
-        if answer == stu3[index]:
-            count3 += 1
- 
-    # 비어있는 answer_list에 count1,2,3를 더해주면 answer_list에 맞춘 정답의 개수가 들어가있음
-    answer_list.append(count1)
-    answer_list.append(count2)
-    answer_list.append(count3)
+    stu1 = [1,2,3,4,5] 
+    stu2 = [2,1,2,3,2,4,2,5] 
+    stu3 = [3,3,1,1,2,2,4,4,5,5] 
     
     result = []
     
-    # 담겨있는 값을 어떻게 꺼낼까 고민했는데
-    # enumerate함수를 이용하여 idex+1을 해주면 순서대로 결과값이 담긴다!
-    for idx,s in enumerate(answer_list):
-        if s == max(answer_list):
+    count = [0,0,0]
+    
+    for index, answer in enumerate(answers) :
+        # enumerate함수를 이용하여, answers에 담긴 index와 answer 값을 하나씩 꺼내서
+        # stu1이 맞췄다면! 하지만 answer가 5개가 아니라 6개, 7개일 수도 있으니까 반복을 돌려야한다.
+        # 1,2,3,4,5 끝나고 또 1,2,3,4,5 앞으로 도니까 이 값을 어떻게 표현하면 좋을까
+        # index % len(stu1)이 정답. 그러면 answers에 6개의 값이 담겨있다고 치면, 5개 돌고나서 나머지 1개는 나눈 값이 0이기때문에 맨앞으로 이동!
+        if answer == stu1[index % len(stu1) ]:
+            count[0] += 1
+        if answer == stu2[index % len(stu2)]:
+            count[1] += 1
+        if answer == stu3[index % len(stu3)]:
+            count[2] += 1
+    
+    for idx, s in enumerate(count):
+        if s == max(count):
+            # 최대값을 담아두고,
+            # 동일한 값이 발생해도 결과값에 담아주니까 그대로 리턴하면 정답이 된다.
             result.append(idx+1)
-            
-    print(result)
     
     
     return result
