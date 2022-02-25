a52d10275ac44645dd8da8679b77fb7ec8f264c8
전은형 <ooooo5h91@gmail.com>
2022-02-25 20:32:16 +0900

    #3. 전화번호목록 해시사용

--- 전화번호목록.py
+++ 전화번호목록.py
@@ -1,14 +1,24 @@
-# 프로그래머스 전화번호목록 (sort와 반복문 사용 + zip() 함수)
+# 프로그래머스 전화번호목록 (hash 사용)
 
 def solution(phone_book):
     
-    # 정렬을 하면 숫자 오름차순으로 정렬되기 때문에
-    # 앞의 수가 뒤의 수의 접두어인가만 확인하면 된다.
-    phone_book.sort()
+    # 1. Hash map 생성
+    hashmap = {}
     
-    # zip()함수를 사용하면 p1이 앞의 수, p2가 뒤의 수
-    for p1, p2 in zip(phone_book, phone_book[1:]):
-        if p2.startswith(p1):
-            return False
+    # 해시의 키값은 번호로, 벨류값은 1로 일단 대입한다.
+    for num in phone_book:
+        hashmap[num] = 1
+    
+    
+    # 2. 전화번호부의 전화번호들을 하나씩 꺼내면서 접두어가 Hash map에 존재하는 지 찾기
+    for phone_number in phone_book:
+        jubdoo = ''
+        # 전화번호 1개의 자릿수들을 하나씩 꺼내면서 비교하기
+        for number in phone_number:
+            # jubdoo에 더해주면 접두가 다시 하나의 전화번호가 된다.
+            jubdoo += number  
+            # 3. 접두어를 찾기(중요 : 내번호와 같은 경우는 제외해야함)
+            if jubdoo in hashmap and jubdoo != phone_number:
+                return False
     
     return True
