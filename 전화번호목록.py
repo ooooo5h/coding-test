331b7470b1ef7c5d7f8770c1577e15b58fb0132c
전은형 <ooooo5h91@gmail.com>
2022-02-25 20:01:36 +0900

    #3. 전화번호목록 반복문 사용 + zip()
    - zip()함수를 이용하면 짝을 지어준다.
    - startswith()는 괄호안의 수로 시작하는지 여부를 판별해준다.

--- 전화번호목록.py
+++ 전화번호목록.py
@@ -1,4 +1,4 @@
-# 프로그래머스 전화번호목록 (sort와 반복문 사용)
+# 프로그래머스 전화번호목록 (sort와 반복문 사용 + zip() 함수)
 
 def solution(phone_book):
     
@@ -6,9 +6,9 @@ def solution(phone_book):
     # 앞의 수가 뒤의 수의 접두어인가만 확인하면 된다.
     phone_book.sort()
     
-    # 앞의 수와 비교하기 위해선 len() -1만 반복문을 돌면 된다.
-    for i in range(len(phone_book)-1):
-        if phone_book[i+1].startswith(phone_book[i]):
+    # zip()함수를 사용하면 p1이 앞의 수, p2가 뒤의 수
+    for p1, p2 in zip(phone_book, phone_book[1:]):
+        if p2.startswith(p1):
             return False
     
     return True
