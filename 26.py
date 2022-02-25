a94a5368b61f1ff86090ec8ada3f6bf1af235986
전은형 <ooooo5h91@gmail.com>
2022-02-25 11:47:35 +0900

    내 풀이 리턴값만 코드 추가
    - 하지만 이렇게 계산하면.. stu1,2,3에 너무 많은 메모리가 낭비되는 것 같다.

--- 26.py
+++ 26.py
@@ -2,14 +2,12 @@
 
 def solution(answers):
     
-    # len(answers)는 너무 막대한 양인것같은데.. 불필요할거같은데 개선을 어떻게 해야할지모르겠음
     stu1 = [1,2,3,4,5] * len(answers)
     stu2 = [2,1,2,3,2,4,2,5] * len(answers)
     stu3 = [3,3,1,1,2,2,4,4,5,5] * len(answers)
     
     answer_list = []
     count1, count2,count3 = 0,0,0
-    
     for index, answer in enumerate(answers) :
         if answer == stu1[index]:
             count1 += 1
@@ -18,19 +16,20 @@ def solution(answers):
         if answer == stu3[index]:
             count3 += 1
  
+    # 비어있는 answer_list에 count1,2,3를 더해주면 answer_list에 맞춘 정답의 개수가 들어가있음
     answer_list.append(count1)
     answer_list.append(count2)
     answer_list.append(count3)
     
+    result = []
     
-    return_list = []
-    best = answer_list.index(max(answer_list))+1
+    # 담겨있는 값을 어떻게 꺼낼까 고민했는데
+    # enumerate함수를 이용하여 idex+1을 해주면 순서대로 결과값이 담긴다!
+    for idx,s in enumerate(answer_list):
+        if s == max(answer_list):
+            result.append(idx+1)
+            
+    print(result)
     
-    return_list.append(best)
-    print(return_list)
     
-    # 최대값이 겹칠땐 어떻게 출력해야할지 모르겠음.. ㅜㅜ
-    if
-    
-    
-    # return answers
+    return result
