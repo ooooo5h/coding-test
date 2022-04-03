934c5178dfcb9e2565589506b33b17e44116d53c
eunhyung <ooooo5h91@gmail.com>
2022-04-03 21:48:55 +0900

    백준 : 3단계 미완성
    - 아직 안 푼 문제 많음

---
+++ baekjoon/3.py
@@ -0,0 +1,50 @@
+# 구구단
+n = int(input())
+for i in range(1, 10) :
+    print(f'{n} * {i} = {n*i}')
+    
+# a + b - 3
+
+t = int(input())
+
+for i in range(t) :
+    a, b = input().split()
+    print(int(a) + int(b))
+
+# 8393
+n = int(input())
+
+answer = 0
+for i in range(1, n+1) :
+    answer += i
+print(answer)
+
+# 2741
+n = int(input())
+for i in range(1,n+1):
+    print(i)
+    
+# 2742
+n = int(input())
+for i in range(n, 0, -1):
+    print(i)
+    
+# 11021
+t = int(input())
+
+for i in range(t):
+    a,b = input().split()
+    print(f'Case #{i+1}: {int(a)+int(b)}')
+    
+# 11022
+t = int(input())
+for i in range(t):
+    a,b = input().split()
+    print(f'Case #{i+1}: {int(a)} + {int(b)} = {int(a)+int(b)}')
+    
+# 10952 => 실패
+a,b = input().split()
+if int(a) != 0 and int(b) != 0 :
+    print(f'{int(a)+int(b)}')
+
+    
