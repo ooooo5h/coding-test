d8dd3512ac069803c70cdec1d8107189803b9e13
eunhyung <ooooo5h91@gmail.com>
2022-04-11 19:54:49 +0900

    7단계 : 2839

---
+++ baekjoon/7단계/2839.py
@@ -0,0 +1,14 @@
+n = int(input())
+box = 0
+
+while True :
+    if n % 5 == 0:
+        box += (n//5)
+        print(box)
+        break
+    
+    n = n - 3
+    box += 1
+    if n < 0 :
+        print('-1')
+        break
